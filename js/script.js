73a38eeec85eb92c5b6aac058f7a3dbb4fb2762a
PaninNet <iliapanin300999@mail.ru>
2021-02-17 14:55:21 +0300

    Проверяю вертску

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function showHide() {
+  let a =document.querySelector('header')
+  a.classList.toggle("activee")
+}
+
+function changeOrder() {
+  let a =document.querySelector('.section_item:first-child')  
+  a.classList.toggle("changeDir")
+}
